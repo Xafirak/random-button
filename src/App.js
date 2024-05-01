ce426dd72fcb2f8352193bd1a9011df67a11ac8f
Xafirak <bensala12@gmail.com>
2024-05-01 16:08:50 +0500

    commit numero 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+
 
 function App() {
+  const allIcons = { ...far, ...fab, ...fas };
+  const iconList = Object.values(allIcons);
+  library.add(iconList);
+  const [currentIcon, setCurrentIcon] = useState({});
+
+  const getRandomIcon = () => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * iconList.length)
+      const randomIcon = iconList[randomNumber];
+      setCurrentIcon(randomIcon);
+    }, 3000);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FontAwesomeIcon className='icon' size='3x' icon={currentIcon} />
+      <button onClick={getRandomIcon}>
+        Click for random icon
+      </button>
+
     </div>
   );
 }
